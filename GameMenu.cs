b68d978e80e3218067c4e1ccaa955924f1858b93
Travis Milne <Travismilne8@gmail.com>
2022-11-15 11:40:26 -0700

    Adding Game Menu

--- GameMenu.cs
+++ GameMenu.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameMenu : MonoBehaviour
 {   
     public GameObject theMenu;
+    public GameObject[] windows;
+    private CharStats[] playerStats;
+
+    public Text[] nameText, hpText, mpText, lvlText, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
+
+    public GameObject[] statusButtons;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +28,63 @@ public class GameMenu : MonoBehaviour
     {
         if(Input.GetButtonDown("Fire2")) {
             if(theMenu.activeInHierarchy) {
-                theMenu.SetActive(false);
-                GameManager.instance.gameMenuOpen = false;
+                //theMenu.SetActive(false);
+                //GameManager.instance.gameMenuOpen = false;
+                CloseMenu();
             } else {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
             }
         }
+    }
+
+    public void UpdateMainStats() {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i=0; i < playerStats.Length; i++) {
+            if(playerStats[i].gameObject.activeInHierarchy) {
+                charStatHolder[i].SetActive(true);
+                nameText[i].text = playerStats[i].charName; 
+                hpText[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP; 
+                mpText[i].text = "MP: " + playerStats[i].currentMP + "/" + playerStats[i].maxMP;
+                lvlText[i].text = "Lvl: " + playerStats[i].playerLevel;
+                expText[i].text = "" + playerStats[i].currentEXP + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel]; 
+                expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].value = playerStats[i].currentEXP;
+                charImage[i].sprite = playerStats[i].charImage;
+            } else {
+                charStatHolder[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ToggleWindow(int windowNumber) {
+        UpdateMainStats();
+        for(int i=0; i < windows.Length; i++) {
+            if(i == windowNumber) {
+                windows[i].SetActive(!windows[i].activeInHierarchy);
+            } else {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+    public void CloseMenu() {
+        for(int i = 0; i<windows.Length; i++) {
+            windows[i].SetActive(false);
+        }
+
+        theMenu.SetActive(false);
+        GameManager.instance.gameMenuOpen = false;
+    }
+
+    public void OpenStatus() {
+        UpdateMainStats();
+        //update the information that is shown
+        for(int i = 0; i < statusButtons.Length; i++) {
+            statusButtons[i].SetActive(playerStats[i].gameObject.activeInHierarchy);
+        }
+
     }
 }
