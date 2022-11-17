332ff6040bc5a3c35821acabd600df667b7b0d80
Travis Milne <Travismilne8@gmail.com>
2022-11-17 14:41:20 -0700

    Shop System started

--- GameMenu.cs
+++ GameMenu.cs
@@ -25,6 +25,8 @@ public class GameMenu : MonoBehaviour
     public GameObject itemCharChoiceMenu;
     public Text[] itemCharChoiceNames;
 
+    public Text goldText;
+
     public static GameMenu instance;
 
 
@@ -68,6 +70,7 @@ public class GameMenu : MonoBehaviour
                 charStatHolder[i].SetActive(false);
             }
         }
+        goldText.text = GameManager.instance.currentGold.ToString() + "g";
     }
 
     public void ToggleWindow(int windowNumber) {
